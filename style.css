b0a58ca20dcf715569e0915a93331a5d4c697f3c
aamanday <amanda.leocadio@escola.pr.gov.br>
2024-08-29 11:30:07 -0300

    Create style.css

---
+++ style.css
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(400, 400);
+  background ("rgb(9,0,0)");
+}
+
+function draw() {
+  fill ("blue")
+  stroke ("white")
+  if(mouseIsPressed) { rect(mouseX, mouseY, 20, 20);}
+  }
